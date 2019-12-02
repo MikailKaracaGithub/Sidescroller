1f8b6b8885363473e11df1dd1859489c02a4900d
Xavaltir <tombis1999@hotmail.com>
2019-12-02 19:32:57 +0100

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using GeoSketch;
+namespace Side_Scroller
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        const int _screenWidth = 800;
+        const int _screenHeight = 800;
+
+        Texture2D _background;
+
+
+        Vector2 _backgroundPos = new Vector2(0, 0);
+        Vector2 _backgroundReset = new Vector2(-700, 0);
+
+        Player player = new Player();
+
+        Song bgSong;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = _screenWidth;
+            graphics.PreferredBackBufferHeight = _screenHeight;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _background = Content.Load<Texture2D>("sky");
+            this.bgSong = Content.Load<Song>("stal");
+            MediaPlayer.Play(bgSong);
+
+        }
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            _backgroundPos.X--;
+            if (_backgroundPos.X < _backgroundReset.X)
+            {
+                _backgroundPos.X = 0;
+            }
+            player.PlayerMovement();
+            base.Update(gameTime);
+
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            spriteBatch.Begin();
+            spriteBatch.Draw(_background, _backgroundPos, Color.White); // background
+            spriteBatch.DrawRectangle(0, 600, 1600, 200, Color.IndianRed, Color.White, 0);// foreground
+            player.DrawPlayer(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+        
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,67 @@
+﻿using GeoSketch;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Side_Scroller
+{
+    class Player
+    {
+        int _speed = 5;
+        int _sprint;
+        Vector2 charPos = new Vector2(200,610);
+
+        bool jumping = false;
+        float startY = 610, jumpspeed = 0;
+
+        public void PlayerMovement()
+        {
+            KeyboardState kState = Keyboard.GetState();
+            if (kState.IsKeyDown(Keys.LeftShift))
+            {
+                _sprint = 2;
+            }else if (kState.IsKeyUp(Keys.LeftShift))
+            {
+                _sprint = 0;
+            }
+            if (kState.IsKeyDown(Keys.D))
+            {
+                charPos.X += _speed + _sprint;
+            }
+            if (kState.IsKeyDown(Keys.A))
+            {
+                charPos.X -= _speed + _sprint;
+            }
+            if (jumping)
+            {
+                charPos.Y += jumpspeed;
+                jumpspeed += 1;
+                if (charPos.Y >= startY)
+            
+                {
+                    charPos.Y = startY;
+                    jumping = false;
+                }
+            }else
+            {
+                if (kState.IsKeyDown(Keys.Space))
+                {
+                    jumping = true;
+                    jumpspeed = -20;//Give it upward thrust
+                }
+            }
+        }
+        public void DrawPlayer(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawRectangle((int)charPos.X, (int) charPos.Y, 75, 100, Color.DarkGray, Color.White, 0); // player
+
+        }
+
+       
+    }
+}
